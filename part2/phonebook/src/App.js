161c053f02a72fec02a08b2fa0b6731f75fd5e43
Peter Sims <ragtimesims@gmail.com>
2019-10-06 11:27:24 -0400

    finish part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,78 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
 import Add from './components/Add';
-import axios from 'axios';
+import Notification from './components/Notification';
+import personsService from './services/persons';
 
 const App = () => {
 	const [ persons, setPersons ] = useState([]);
 	const [ newName, setNewName ] = useState('');
 	const [ newNumber, setNewNumber ] = useState('');
 	const [ newFilter, setNewFilter ] = useState('');
+	const [ notification, setNotification ] = useState(null);
+	const [ styleType, setStyleType ] = useState(null);
 
 	useEffect(() => {
-		axios.get('http://localhost:3001/persons').then((response) => {
-			setPersons(response.data);
+		personsService.getAll().then((response) => {
+			setPersons(response);
 		});
 	}, []);
 
 	const addEntry = (e) => {
 		e.preventDefault();
-		if (!persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())) {
-			setPersons(persons.concat({ name: newName, number: newNumber }));
+		if (persons.find((person) => person.name === newName)) {
+			let entry = persons.find((person) => person.name === newName);
+			let confirmUpdate = window.confirm(`Do you want to update the phone number for ${entry.name}?`);
+			if (confirmUpdate) {
+				const newPerson = { name: newName, number: newNumber };
+
+				personsService.update(entry.id, newPerson).then((res) => {
+					setPersons(persons.map((person) => (person.id === res.id ? res : person)));
+				});
+
+				setNotification(`${newName}'s number has been updated`);
+				setStyleType('notification');
+				setTimeout(() => {
+					setNotification(null);
+					setStyleType(null);
+				}, 5000);
+				setNewName('');
+				setNewNumber('');
+			}
+		} else if (!persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())) {
+			const newPerson = { name: newName, number: newNumber };
+
+			personsService.create(newPerson).then((res) => {
+				setPersons(persons.concat(res));
+			});
+
+			setNotification(`${newName} has been added `);
+			setStyleType('notification');
+			setTimeout(() => {
+				setNotification(null);
+				setStyleType(null);
+			}, 5000);
+
 			setNewName('');
 			setNewNumber('');
 		} else {
-			alert(`${newName} is already added to phonebook`);
+			setNotification(`${newName} already exists `);
+			setStyleType('error');
+			setTimeout(() => {
+				setNotification(null);
+				setStyleType(null);
+			}, 5000);
+		}
+	};
+
+	const handleDelete = (id) => {
+		const entry = persons.find((person) => person.id === id);
+		let deleteAlert = window.confirm(`Are you sure you want to delete the entry ${entry.name}?`);
+
+		if (deleteAlert) {
+			personsService.remove(id).then(() => {
+				setPersons(persons.filter((person) => person.id !== id));
+			});
 		}
 	};
 
@@ -41,6 +91,7 @@ const App = () => {
 
 	return (
 		<div>
+			<Notification message={notification} type={styleType} />
 			<Filter value={newFilter} onChange={handleFilterChange} />
 			<Add
 				addEntry={addEntry}
@@ -49,7 +100,7 @@ const App = () => {
 				newNumber={newNumber}
 				handleNumberChange={handleNumberChange}
 			/>
-			<Persons persons={persons} newFilter={newFilter} />
+			<Persons persons={persons} newFilter={newFilter} handleDelete={handleDelete} />
 		</div>
 	);
 };
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Persons = ({ persons, newFilter }) => {
+const Persons = ({ persons, newFilter, handleDelete }) => {
 	const rows = () =>
 		persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())).map((person) => (
 			<div key={person.name}>
-				{person.name} {person.number}
+				{person.name} {person.number} <button onClick={() => handleDelete(person.id)}>delete</button>
 			</div>
 		));
 	return (
