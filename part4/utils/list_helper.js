931469640efcb254f5589ebf524053d2c2ee7d82
Peter Sims <ragtimesims@gmail.com>
2019-10-08 11:01:07 -0400

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,25 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const highestLikes = (blogs) => {
+	if (!blogs) return 0;
+
+	let likedBlog = blogs[0];
+	blogs.forEach((blog) => {
+		if (blog.likes > likedBlog.likes) {
+			likedBlog = blog;
+		}
+	});
+
+	return {
+		title: likedBlog.title,
+		author: likedBlog.author,
+		likes: likedBlog.likes
+	};
+};
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	highestLikes
 };
