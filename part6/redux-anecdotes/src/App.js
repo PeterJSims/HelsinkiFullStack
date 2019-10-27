12fca34ab1af0c30c373bba5723f32d46708a659
Peter Sims <ragtimesims@gmail.com>
2019-10-27 12:40:32 -0400

    also 6.15!

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,7 +4,7 @@ import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
 
-const App = ({ store }) => {
+const App = () => {
 	return (
 		<div>
 			<h2>Anecdotes</h2>
