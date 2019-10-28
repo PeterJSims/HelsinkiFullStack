ca150422a1fb4cd7cb84b58a1d6f68e41236aded
Peter Sims <ragtimesims@gmail.com>
2019-10-28 13:18:15 -0400

    index fix

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import store from './store';
 import App from './App';
-import anecdoteReducer from './reducers/anecdoteReducer';
-import notificationReducer from './reducers/notificationReducer';
-import filterReducer from './reducers/filterReducer';
 
-const reducer = combineReducers({
-	anecdotes: anecdoteReducer,
-	notification: notificationReducer,
-	filter: filterReducer
-});
-
-const store = createStore(reducer);
-
-const render = () => {
-	ReactDOM.render(
-		<Provider store={store}>
-			<App />
-		</Provider>,
-		document.getElementById('root')
-	);
-};
-
-render();
-store.subscribe(render);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
